190379c56a0e47433ef2c1d1ff0bd7b486c7a810
Richard Conway <richard@rconway.co.uk>
2019-07-01 15:14:27 +0000

    Simplified goapp for a single '/data' endpoint.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("zzz STARTING...")
 
 	mux := http.NewServeMux()
 
@@ -15,35 +15,13 @@ func main() {
 		http.NotFound(w, r)
 	})
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit /")
-		http.SetCookie(w, &http.Cookie{Name: "c1", Value: "root2", Path: "/fred"})
-		http.Redirect(w, r, "https://goapp.rconway.co.uk/larry", 307)
-	})
-
-	mux.Handle("/larry", http.RedirectHandler("https://goapp.rconway.co.uk/bob", 307))
-
-	mux.HandleFunc("/bob", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit /bob")
-		http.SetCookie(w, &http.Cookie{Name: "c2", Value: "bob"})
-		http.Redirect(w, r, "https://goapp.rconway.co.uk/fred", 307)
-	})
-
-	mux.HandleFunc("/fred", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("hit /fred")
+	mux.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hit /data")
 		//w.WriteHeader(204)
-		w.Write([]byte("<body><h6 style=\"text-align: center;\">TBD - result is here</h6></body>"))
+		w.Write([]byte("<body><h5 style=\"text-align: center;\">You got some DATA</h5></body>"))
 	})
 
-	mux.HandleFunc("/richard", func(w http.ResponseWriter, r *http.Request) {
-		requestURL := r.URL
-		requestURI := r.RequestURI
-		fmt.Println("URL=", r.URL)
-		fmt.Println("requestURL=", requestURL.String())
-		fmt.Println("requestURI=", requestURI)
-		w.Write([]byte("this is richard"))
-	})
-
+	fmt.Println("LISTEN on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 
 	fmt.Println("EXITING...")
