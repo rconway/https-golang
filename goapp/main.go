a498934e0446e9d13d063e05706eba0e6f51e5d5
Richard Conway <richard@rconway.co.uk>
2019-07-01 20:31:41 +0000

    Run goapp under a '/goapp/' url prefix.

--- goapp/main.go
+++ goapp/main.go
@@ -15,10 +15,31 @@ func main() {
 		http.NotFound(w, r)
 	})
 
+	mux.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hit /index")
+		http.Redirect(w, r, "data", 307)
+	})
+
 	mux.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hit /data")
 		//w.WriteHeader(204)
-		w.Write([]byte("<body><h5 style=\"text-align: center;\">You got some DATA</h5></body>"))
+		w.Write([]byte(`
+			<body>
+				<h5 style="text-align: center;">You got some DATA</h5>
+				<a href="info">INFO</a>
+			</body>
+		`))
+	})
+
+	mux.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hit /info")
+		//w.WriteHeader(204)
+		w.Write([]byte(`
+			<body>
+				<h5 style="text-align: center;">You got some INFO</h5>
+				<a href="data">DATA</a>
+			</body>
+		`))
 	})
 
 	fmt.Println("LISTEN on port 8080...")
